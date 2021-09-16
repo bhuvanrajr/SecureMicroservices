b709f7ff137ba008ae09cd3bb23e39dd562a9273
ES-AREA1\HSE8 <bhuvanrajr@eurofins.com>
2021-09-16 18:51:45 +0530

    Add project files.

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,36 @@
+﻿using IdentityServer4.Models;
+using IdentityServer4.Test;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+            {
+                new Client
+                {
+                    ClientId="movieClient",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "movieAPI" }
+                }
+            };
+        public static IEnumerable<IdentityResource> IdentityResources => new IdentityResource[] { };
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[] { };
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+            {
+                new ApiScope("movieAPI","Movie API")
+            };
+        public static List<TestUser> TestUsers => new List<TestUser> { };
+
+    }
+}
---
+++ Movies.Client/API Services/MovieApiService.cs
@@ -0,0 +1,46 @@
+﻿using Movies.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Movies.Client.API_Services
+{
+    public class MovieApiService : IMovieApiService
+    {
+        public Task<Movie> CreateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Movie> GetMovie(string id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Movie>> GetMovies()
+        {
+            List<Movie> movies =  new List<Movie>() {
+                    new Movie {
+                        Id = 1,
+                        Genre = "Action",
+                        ImageUrl = "Images",
+                        Owner = "Bhuvan",
+                        Rating = "5",
+                        ReleaseDate = DateTime.Today,
+                        Title = "Bahadhoor Gandu" }
+            };
+
+            return await Task.FromResult(movies);
+        }
+
+        public Task<Movie> UpdateMovie(Movie movie)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
